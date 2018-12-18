3e449db1d353d1b18958babd7f6962ee91204997
Darren Henderson <darrenh@ccsj.edu>
2018-12-18 11:56:56 -0600

    Working as of page 702.

---
+++ UsingViewComponents/Components/CitySummary.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UsingViewComponents.Models;
+
+namespace UsingViewComponents.Components
+{
+    public class CitySummary : ViewComponent
+    {
+        private ICityRepository repository;
+
+        public CitySummary(ICityRepository repo)
+        {
+            repository = repo;
+        }
+
+        public string Invoke()
+        {
+            return $"{repository.Cities.Count()} cities, "
+                + $"{repository.Cities.Sum(c => c.Population)} people";
+        }
+
+            //public IViewComponentResult Invoke(bool showList)
+            //{
+            //    if (showList)
+            //    {
+            //        return View("CityList", repository.Cities);
+            //    }
+            //    else
+            //    {
+            //        return View(new CityViewModel
+            //        {
+            //            Cities = repository.Cities.Count(),
+            //            Population = repository.Cities.Sum(c => c.Population)
+            //        });
+            //    }
+            //}
+        }
+}
